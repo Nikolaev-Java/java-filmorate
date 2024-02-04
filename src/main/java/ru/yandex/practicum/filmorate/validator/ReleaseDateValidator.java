5273c9571c109973d999c9e5cd3beecdc0e08b57
Denis <epselent-dev@yandex.ru>
2024-02-04 17:16:01 +0500

    style: рефактор. Попытка пройти cod style часть 3

--- src/main/java/ru/yandex/practicum/filmorate/validator/ReleaseDateValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/validator/ReleaseDateValidator.java
@@ -2,16 +2,12 @@ package ru.yandex.practicum.filmorate.validator;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
-import java.text.DateFormat;
-import java.text.Format;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.Formatter;
 
 public class ReleaseDateValidator implements ConstraintValidator<ReleaseDate, LocalDate> {
     @Override
     public boolean isValid(LocalDate localDate, ConstraintValidatorContext constraintValidatorContext) {
 
-        return !localDate.isBefore(LocalDate.of(1895,12,28));
+        return !localDate.isBefore(LocalDate.of(1895, 12, 28));
     }
 }
